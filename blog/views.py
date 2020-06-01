f5e52342fdb5e843532e60d7acab7ba61210bbed
thecalmguy <mittalmanik1998@gmail.com>
2020-06-02 03:58:51 +0530

    changed the html for the site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+def boss_list(request):
+    return render(request, 'boss_list.html', {})
